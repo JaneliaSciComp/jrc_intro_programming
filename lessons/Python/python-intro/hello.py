11c85f5f9226b505d2dc7091a07e103d0a8bfd3e
Charlotte Weaver <weaverc10@janelia.hhmi.org>
2017-04-10 17:01:07 -0400

    Updated to python 3 with some additonal explanations where necessary

--- lessons/Python/python-intro/hello.py
+++ lessons/Python/python-intro/hello.py
@@ -1,16 +1,16 @@
 import time
 
-print "hello world"
+print("hello world")
 
 time.sleep(1)
 
-print "Dr. Grace Hopper, 1906-1992, creator of the first compiler"
+print("Dr. Grace Hopper, 1906-1992, creator of the first compiler")
 
 time.sleep(2)
 
 f = open("hopper.txt")
 for line in f:
-  print line.rstrip()
+  print(line.rstrip())
   time.sleep(0.2)
 
 f.close()
